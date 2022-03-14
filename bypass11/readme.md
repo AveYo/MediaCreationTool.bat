0b1953ce1ae30eaba672682dbb8504fa172a9d71
AveYo <aveyo@yahoo.com>
2022-03-14 16:44:19 +0200

    windows_update_refresh
    use if having update issues on any ver

---
+++ bypass11/readme.md
@@ -0,0 +1,4 @@
+If having update issues - on any windows version  
+------------------------------------------------  
+Use [windows_update_refresh.bat](bypass11/windows_update_refresh.bat)  
+_AveYo: as a bonus, it hides unsupported PC nags_  
---
+++ bypass11/windows_update_refresh.bat
@@ -0,0 +1,32 @@
+@(set "0=%~f0"^)#) & powershell -nop -c iex([io.file]::ReadAllText($env:0)) & exit /b
+
+$_Paste_in_Powershell = {
+  $host.ui.rawui.windowtitle = 'WINDOWS UPDATE REFRESH'
+  bitsadmin /reset /allusers
+  'wuauserv','bits','cryptSvc','usosvc' |% { start -win 1 net1 "stop $_" }
+  sleep 5
+  'wuauserv','bits','cryptSvc','usosvc' |% {
+    $svc=tasklist /svc /fi "services eq $_" /fo csv | convertfrom-csv; if ($svc.PID -gt 10) {kill $svc.PID -force -ea 0}
+  }
+  'trustedinstaller','tiworker','wuauclt','sihclient','usoclient','mousocoreworker','systemsettings' |% {
+    kill -name $_ -force -ea 0
+  }
+  cmd /c rd /s /q "%SystemDrive%\`$WINDOWS.~BT\Sources\Panther"
+  cmd /c rd /s /q "%SystemRoot%\system32\catroot2"
+  cmd /c rd /s /q "%SystemRoot%\SoftwareDistribution"
+  cmd /c del /f /q "%SystemRoot%\Logs\WindowsUpdate\*"
+  cmd /c del /f /q "%ProgramData%\USOPrivate\UpdateStore\*"
+  cmd /c del /s /f /q "%ProgramData%\USOShared\Logs\*"
+  cmd /c rd /s /q "%ProgramFiles%\UNP"
+  try { Get-WindowsUpdateLog -LogPath $env:temp\temp.log -ForceFlush -Confirm:$False -ea 0 } catch {}
+  <# AveYo: comment 3 lines below to NOT hide 11 unsupported/upgrade nag - 25H1 is NOT a typo #>
+  reg add "HKCU\Control Panel\UnsupportedHardwareNotificationCache" /v "SV2" /d 0 /t reg_dword /f
+  reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "TargetReleaseVersion" /d 1 /t reg_dword /f
+  reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "TargetReleaseVersionInfo" /d 25H1 /f
+  net1 start wuauserv; net1 start bits; net1 start usosvc
+  UsoClient RefreshSettings
+  sleep 5
+  start ms-settings:windowsupdate
+  #timeout -1
+} ; start -verb runas powershell -args "-nop -c & {`n`n$($_Paste_in_Powershell -replace '"','\"')}"
+$_Press_Enter
