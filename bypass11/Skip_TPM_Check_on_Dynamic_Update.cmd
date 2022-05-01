ebaf723010b9c546981106385cc0f6c2a3de4c04
AveYo <aveyo@yahoo.com>
2022-05-01 20:01:25 +0000

    V8
    it seems all those billions poured into telemetry and machine learning by microsoft, google and av vendors are only used to "classify" user's private data,  
    none trickled down into av heuristics so av's today are dumber than a rock, not discerning actually malicious stuff, even below the 90's era signature level. 
    in the case of microsoft there's also a strong fake positive bias!
    
    script restructured around cmd, and a bit more verbose and spaced out, but the gist of it is the same: IFEO intercept and add /Product Server option
    notable change is that it now exclusively focuses setup via Windows Update
    you're gonna have to bypass manual 11 setup some other way - hint: cd.>sources\appraiserres.dll

--- bypass11/Skip_TPM_Check_on_Dynamic_Update.cmd
+++ bypass11/Skip_TPM_Check_on_Dynamic_Update.cmd
@@ -1,52 +1,59 @@
-@(set "0=%~f0"^)#) & powershell -nop -c iex([io.file]::ReadAllText($env:0)) & exit/b
-#:: double-click to run or just copy-paste into powershell - it's a standalone hybrid script
+@(set '(=)||' <# lean and mean cmd / powershell hybrid #> @'
 
-#:: v7 dynamically skips the anti-consumer windows 11 setup checks via /Product Server trick  
-#:: it is most reliable, and only has a 'Windows Server' label cosmetic-ish difference
-#:: works with:
-#:: 11 setup via Windows Update (after using OfflineInsiderEnroll by whatever127 and abbodi1406)
-#:: 11 setup via mounted iso / usb (use the Quick.. script for skipping 11 setup checks at boot)
+::# get 11 via Windows Update on 'unsupported' PC, with /Product Server dynamic trick
+::# if wu download stuck at 0% use OfflineInsiderEnroll by whatever127 and abbodi1406
+::# V8: av false & fake positive? reject humanity (powershell), return to monke (cmd) 
 
-$_Paste_in_Powershell = { $Code = @'
- $Nfo = 'Skip TPM Check on Dynamic Update v7, AveYo 2021'
- $Arg = (([environment]::get_CommandLine()-split'-[-]% ')[1]-split'.exe[\p{P}]? ')[1]
- foreach ($x in 'Product','DynamicUpdate','Telemetry') {$Arg = $Arg -replace $('\p{P}?/'+ $x +'\p{P}? \p{P}?[A-Z]+\p{P}? '),' '}
- $Cli = ' /DynamicUpdate Disable /Telemetry Disable ' + $Arg; $Srv = ' /Product Server' + $Cli
- $Dir = join-path $([Environment]::SystemDirectory[0..2]-join'') '$WINDOWS.~BT\Sources\'
- $Cfg = join-path $Dir 'EI.cfg'; $EI = '[Channel]' +[char]13+[char]10+ '_Default' +[char]13+[char]10
- $Exe = join-path $Dir 'SetupHost.exe'; $Inf = get-item -force -lit $Exe; [int]$Ver = $Inf.VersionInfo.FileBuildPart
- if ($Ver -ge 22000) {$Run = $Exe + $Srv} else {$Run = $Exe + $Cli}
- if ($Ver -ge 22000 -and !(test-path $Cfg)) {[io.file]::WriteAllText($Cfg, $EI)}
+@echo off & title get 11 via Windows Update on 'unsupported' PC || AveYo 2022
+if /i "%~f0" neq "%ProgramData%\get11.cmd" goto setup
+set CLI=%*& set SOURCES=%SystemDrive%\$WINDOWS.~BT\Sources&;
+if not defined CLI (exit /b) else if not exist %SOURCES%\SetupHost.exe (exit /b)
+if not exist %SOURCES%\SetupCore.exe mklink /h %SOURCES%\SetupCore.exe %SOURCES%\SetupHost.exe 
+powershell -win 1 -nop -c ";"
+set /a restart_application=0x800705BB & call set CLI=%%CLI:%1 =%%&;
+set /a incorrect_parameter=0x80070057 & set SRV=%CLI:/Product Client =%&; 
+set /a launch_option_error=0xc190010a & set SRV=/Product Server /Telemetry Disable /Compat IgnoreWarning %SRV:/Product Server =%&;
+for /f %%W in ('tasklist /nh /fi "imagename eq setupprep.exe"') do if /i %%W==setupprep.exe set SRV=%CLI%&;
+%SOURCES%\SetupCore.exe %SRV% 
+if %errorlevel% == %restart_application% %SOURCES%\SetupCore.exe %SRV%
+exit /b
 
- $D=@(); $T=@(); $A=@(); $M=[AppDomain]::CurrentDomain.DefineDynamicAssembly(1,1).DefineDynamicModule(1) 
- foreach ($x in 0..2) {$D+=$M.DefineType('AveYo_'+$x,1179913,[ValueType])}; foreach ($x in 1..2) {$D+=$D[$x].MakeByRefType()}
- $S=[string]; $I=[int32]; $U=[uintptr]; $y=0; $z=0;  foreach ($x in $U,$U,$I,$I) {$9=$D[2].DefineField('f'+$y++,$x,6)}
- foreach ($x in $I,$S,$S,$S,$I,$I,$I,$I,$I,$I,$I,$I,[int16],[int16],$U,$U,$U,$U) {$9=$D[1].DefineField('f'+$z++,$x,6)}
- $9=$D[0].DefinePInvokeMethod('CreateProcess','kernel32',8214,1,[void],($S,$S,$I,$I,[bool],$I,$I,$S,$D[3],$D[4]),1,4)
- $9=$D[0].DefinePInvokeMethod('DebugActiveProcessStop','kernel32',8214,1,[void],($I),1,4)
- foreach ($x in 0..2) {$T+=$D[$x].CreateType()}; foreach ($x in 1..2) {$A+=[Activator]::CreateInstance($T[$x])}
- $R=$null, $Run, $null, $null, $false, 0x02000011, $null, $null, $A[0], $A[1] 
- $T[0].GetMethod('CreateProcess').invoke(0, $R); $T[0].GetMethod('DebugActiveProcessStop').invoke(0, $R[9].f2)
- $W=get-process -pid $R[9].f2 -ea 0; for (;;) {sleep 1; if (0-eq $R[9].f2 -or $null-eq $W -or $W.HasExited) {return} }
-'@ -replace '\r?\n|\r', '; ' <# lines 20-29 are needed for escaping ifeo, remain calm ;) #>  
+:setup
+::# elevate with native shell by AveYo
+>nul reg add hkcu\software\classes\.Admin\shell\runas\command /f /ve /d "cmd /x /d /r set \"f0=%%2\"& call \"%%2\" %%3"& set _= %* 
+>nul fltmc|| if "%f0%" neq "%~f0" (cd.>"%temp%\runas.Admin" & start "%~n0" /high "%temp%\runas.Admin" "%~f0" "%_:"=""%" & exit /b)
 
- $IFEO = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\SetupHost.exe'
- $Prog = join-path $([Environment]::SystemDirectory[0..2] -join '') '$WINDOWS.~BT\Sources\SetupHost.exe'
- $Skip = "powershell -win 1 -nop -c iex (get-itemproperty '$IFEO\0' 'Code' -ea 0).Code; write-host --%"
- if (test-path "$IFEO\0") {
-   remove-item $IFEO -rec -force -ea 0 >''
-   write-host -fore 0xf -back 0xd "`n Skip TPM Check on Dynamic Update v7 [REMOVED] run again to install " 
- } else {                              
-   new-item "$IFEO\0" -force -ea 0 >'' 
-   set-itemproperty "$IFEO\0" 'Debugger' $Skip -force -ea 0; set-itemproperty "$IFEO\0" 'Code' $Code -force -ea 0
-   set-itemproperty "$IFEO\0" 'FilterFullPath' $Prog -force -ea 0; set-itemproperty $IFEO 'UseFilter' 1 -type dword -force -ea 0
-   write-host -fore 0xf -back 0x2 "`n Skip TPM Check on Dynamic Update v7 [INSTALLED] run again to remove "
- } 
- remove-item $($IFEO -replace 'SetupHost', 'vdsldr') -rec -force -ea 0 >''; rmdir (split-path $Prog) -rec -force -ea 0 >''
- $N = 'Skip TPM Check on Dynamic Update' <# also remove wmi-based v1 if somehow still installed, not just vdsldr-based v2 - v5 #>
- $U = 'root\subscription'; $C = gwmi -Class CommandLineEventConsumer -Namespace $U -Filter "Name='$N'" -ea 0 
- $B = gwmi -Class __FilterToConsumerBinding -Namespace $U -Filter "Filter = ""__eventfilter.name='$N'""" -ea 0
- $F = gwmi -Class __EventFilter -NameSpace $U -Filter "Name='$N'" -ea 0; $B,$C,$F |% {$_|rwmi -ea 0}; timeout /t 5
-} ; start -verb runas powershell -args "-nop -c & {`n`n$($_Paste_in_Powershell-replace'"','\"')}"
-$_Press_Enter
-#::
+::# lean xp+ color macros by AveYo:  %<%:af " hello "%>>%  &  %<%:cf " w\"or\"ld "%>%   for single \ / " use .%|%\  .%|%/  \"%|%\"
+for /f "delims=:" %%s in ('echo;prompt $h$s$h:^|cmd /d') do set "|=%%s"&set ">>=\..\c nul&set /p s=%%s%%s%%s%%s%%s%%s%%s<nul&popd"
+set "<=pushd "%appdata%"&2>nul findstr /c:\ /a" &set ">=%>>%&echo;" &set "|=%|:~0,1%" &set /p s=\<nul>"%appdata%\c"
+
+::# toggle when launched without arguments, else jump to arguments: "install" or "remove" 
+set CLI=%*& set IFEO=HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options&;
+wmic /namespace:"\\root\subscription" path __EventFilter where Name="Skip TPM Check on Dynamic Update" delete >nul 2>nul & rem v1
+reg delete "%IFEO%\vdsldr.exe" /f 2>nul & rem v2 - v5
+if /i "%CLI%"=="" reg query "%IFEO%\SetupHost.exe\0" /v Debugger >nul 2>nul && goto remove || goto install
+if /i "%~1"=="install" (goto install) else if /i "%~1"=="remove" goto remove
+
+:install
+@prompt $H & echo on
+copy /y "%~f0" "%ProgramData%\get11.cmd"
+reg add "%IFEO%\SetupHost.exe" /f /v UseFilter /d 1 /t reg_dword
+reg add "%IFEO%\SetupHost.exe\0" /f /v FilterFullPath /d "%SystemDrive%\$WINDOWS.~BT\Sources\SetupHost.exe"
+reg add "%IFEO%\SetupHost.exe\0" /f /v Debugger /d "%ProgramData%\get11.cmd"
+@echo off & echo;
+%<%:2f " Skip TPM Check on Dynamic Update v8 [INSTALLED] run again to remove "%>%
+if /i "%CLI%"=="" timeout /t 7
+exit /b
+
+:remove
+@prompt $H & echo on
+del /f /q "%ProgramData%\get11.cmd"
+reg delete "%IFEO%\SetupHost.exe" /f 
+@echo off & echo;
+%<%:df " Skip TPM Check on Dynamic Update v8 [REMOVED] run again to install "%>%
+if /i "%CLI%"=="" timeout /t 7
+exit /b 
+
+'@); $0 = "$env:temp\Skip_TPM_Check_on_Dynamic_Update.cmd"; ${(=)||} | out-file $0 -encoding default -force; & $0
+# press enter
+ 
