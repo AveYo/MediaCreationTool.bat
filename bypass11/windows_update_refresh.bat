7039e34f263b3f8e1c0437ecae2da5d9d62e1170
AveYo <aveyo@yahoo.com>
2022-05-03 14:36:15 +0000

    Update windows_update_refresh.bat

--- bypass11/windows_update_refresh.bat
+++ bypass11/windows_update_refresh.bat
@@ -1,33 +1,43 @@
-@(set "0=%~f0"^)#) & powershell -nop -c iex([io.file]::ReadAllText($env:0)) & exit /b
+@(set '(=)||' <# lean and mean cmd / ps1 hybrid, can paste into powershell console #> @'
 
-$_Paste_in_Powershell = {
-  $host.ui.rawui.windowtitle = 'WINDOWS UPDATE REFRESH'
-  bitsadmin /reset /allusers
-  'wuauserv','bits','cryptSvc','usosvc' |% { start -win 1 net1 "stop $_" }
-  sleep 5
-  'wuauserv','bits','cryptSvc','usosvc' |% {
-    $svc=tasklist /svc /fi "services eq $_" /fo csv | convertfrom-csv; if ($svc.PID -gt 10) {kill $svc.PID -force -ea 0}
-  }
-  'trustedinstaller','tiworker','wuauclt','sihclient','usoclient','mousocoreworker','setuphost','systemsettings' |% {
-    kill -name $_ -force -ea 0
-  }
-  cmd /c rd /s /q "%SystemDrive%\`$WINDOWS.~BT\Sources\Panther"
-  cmd /c rd /s /q "%SystemRoot%\system32\catroot2" # because kill $svc.PID ;)
-  cmd /c rd /s /q "%SystemRoot%\SoftwareDistribution"
-  cmd /c del /f /q "%SystemRoot%\Logs\WindowsUpdate\*"
-  cmd /c del /f /q "%ProgramData%\USOPrivate\UpdateStore\*" # clearing USO suggested by abbodi1406 @ MDL
-  cmd /c del /s /f /q "%ProgramData%\USOShared\Logs\*"
-  cmd /c rd /s /q "%ProgramFiles%\UNP"
-  try { Get-WindowsUpdateLog -LogPath $env:temp\temp.log -ForceFlush -Confirm:$False -ea 0 } catch {}
-  <# AveYo: comment reg lines below to NOT hide 11 unsupported/upgrade nag #>
-  reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "TargetReleaseVersionInfo" /d 25H1 /f # 25H1 NOT a typo ;)
-  reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "TargetReleaseVersion" /d 1 /t reg_dword /f
-  reg add "HKCU\Control Panel\UnsupportedHardwareNotificationCache" /v "SV2" /d 0 /t reg_dword /f # hide desktop watermark
-  reg add "HKCU\Control Panel\UnsupportedHardwareNotificationCache" /v "SV1" /d 0 /t reg_dword /f # suggested by awuctl @ MDL
-  net1 start wuauserv; net1 start bits; net1 start usosvc
-  UsoClient RefreshSettings
-  sleep 5
-  start ms-settings:windowsupdate
-  #timeout -1
-} ; start -verb runas powershell -args "-nop -c & {`n`n$($_Paste_in_Powershell -replace '"','\"')}"
-$_Press_Enter
+@echo off & title WINDOWS UPDATE REFRESH
+
+::# elevate with native shell by AveYo
+>nul reg add hkcu\software\classes\.Admin\shell\runas\command /f /ve /d "cmd /x /d /r set \"f0=%%2\"& call \"%%2\" %%3"& set _= %* 
+>nul fltmc|| if "%f0%" neq "%~f0" (cd.>"%temp%\runas.Admin" & start "%~n0" /high "%temp%\runas.Admin" "%~f0" "%_:"=""%" & exit /b)
+
+::# stop pending updates
+for /f "tokens=6 delims=[]. " %%b in ('ver') do set /a BUILD=%%b
+set KILL=& set UPDATE=windowsupdatebox updateassistant updateassistantcheck windows10upgrade windows10upgraderapp 
+for %%w in (dism setuphost tiworker usoclient sihclient wuauclt culauncher sedlauncher osrrb ruximics ruximih disktoast eosnotify 
+  musnotification musnotificationux musnotifyicon monotificationux mousocoreworker %UPDATE%) do call set KILL=/im %%w.exe %%KILL%%
+taskkill /f %KILL% 2>nul & dism /cleanup-wim & bitsadmin /reset /allusers 
+for %%w in (msiserver wuauserv bits usosvc dosvc cryptsvc) do start /min cmd /d /x /c net stop %%w /y 
+taskkill /f %KILL% /im systemsettings.exe 2>nul & timeout 7 /nobreak >nul
+for /f tokens^=3^,5^ delims^=^" %%X in ('tasklist /fo csv /nh /svc /fi "services eq wuauserv"') do (
+  taskkill /f /pid %%X & for %%w in (%%Y) do if /i %%w neq wuauserv if /i %%w neq bits if /i %%w neq usosvc net start %%w /y 
+)
+
+::# clear update logs
+set "DATA=%ProgramData%" & set "LOG=%SystemRoot%\Logs\WindowsUpdate" & set "SRC=%SystemDrive%\$WINDOWS.~BT\Sources" 
+del /f /s /q "%DATA%\USOShared\Logs\*" "%DATA%\USOPrivate\UpdateStore\*" "%DATA%\Microsoft\Network\Downloader\*" 2>nul
+powershell -nop -c "try {$null=Get-WindowsUpdateLog -LogPath $env:temp\WU.log -ForceFlush -Confirm:$false -ea 0} catch {}" >nul
+rmdir /s /q "%LOG%" "%ProgramFiles%\UNP" "%SystemRoot%\SoftwareDistribution" "%SystemDrive%\Windows.old\Cleanup"
+if exist %SRC%\setuphost.exe if exist %SRC%\setupprep.exe start "cleanup" /wait %SRC%\setupprep.exe /cleanup /quiet
+
+::# remove forced upgraders and update remediators 
+call "%SystemRoot%\UpdateAssistant\Windows10Upgrade.exe" /ForceUninstall 2>nul
+call "%SystemDrive%\Windows10Upgrade\Windows10UpgraderApp.exe" /ForceUninstall 2>nul
+msiexec /X {1BA1133B-1C7A-41A0-8CBF-9B993E63D296} /qn 2>nul & echo;Removed osrss
+msiexec /X {8F2D6CEB-BC98-4B69-A5C1-78BED238FE77} /qn 2>nul & echo;Removed rempl, ruxim
+msiexec /X {0746492E-47B6-4251-940C-44462DFD74BB} /qn 2>nul & echo;Removed CUAssistant
+msiexec /X {76A22428-2400-4521-96AF-7AC4A6174CA5} /qn 2>nul & echo;Removed UpdateAssistant & echo;
+
+::# start update services 
+net start bits /y & net start wuauserv /y & net start usosvc /y 2>nul & start /min UsoClient RefreshSettings
+echo;AveYo: run again / reboot if there are still pending files or services & pause
+exit /b
+
+'@); $0 = "$env:temp\windows_update_refresh.bat"; ${(=)||} | out-file $0 -encoding default -force; & $0
+ # press enter
+ 
