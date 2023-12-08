c33e7f88e015c39975511e1649d1a1f153b23c07
AveYo <aveyo@yahoo.com>
2023-12-08 10:29:48 +0200

    2023.12.07 V13
    skip 2nd tpm check on Canary iso (above 25977)

--- bypass11/Skip_TPM_Check_on_Dynamic_Update.cmd
+++ bypass11/Skip_TPM_Check_on_Dynamic_Update.cmd
@@ -2,9 +2,9 @@
 
 ::# Get 11 on 'unsupported' PC via Windows Update or mounted ISO (no patching needed)
 ::# if WU is stuck use windows_update_refresh.bat; Beta/Dev/Canary needs OfflineInsiderEnroll
-::# V12: refined method, no Server label; future proofing; tested with 25905 iso, wu and the new wu repair version  
+::# V13: skip 2nd tpm check on Canary iso; no Server label; future proofing; tested with 26010 iso, wu and wu repair version
 
-@echo off & title get 11 on 'unsupported' PC || AveYo 2023.07.14
+@echo off & title get 11 on 'unsupported' PC || AveYo 2023.12.07
 if /i "%~f0" neq "%SystemDrive%\Scripts\get11.cmd" goto setup
 powershell -win 1 -nop -c ";"
 set CLI=%*& set SOURCES=%SystemDrive%\$WINDOWS.~BT\Sources& set MEDIA=.& set MOD=CLI& set PRE=WUA& set /a VER=11
@@ -22,11 +22,22 @@ if %VER% == 11 for %%W in ("%MEDIA%appraiserres.dll") do if exist %%W if %%~zW =
 if %VER% == 11 findstr /r "P.r.o.d.u.c.t.V.e.r.s.i.o.n...1.0.\..0.\..2.[2-9]" %SOURCES%\SetupHost.exe >nul 2>nul || set /a VER=10
 if %VER% == 11 if not exist "%MEDIA%EI.cfg" (echo;[Channel]>%SOURCES%\EI.cfg & echo;_Default>>%SOURCES%\EI.cfg)
 if %VER%_%PRE% == 11_ISO (%SOURCES%\WindowsUpdateBox.exe /Product Server /PreDownload /Quiet %OPT%)
-if %VER%_%PRE% == 11_ISO (del /f /q %SOURCES%\appraiserres.dll 2>nul & cd.>%SOURCES%\appraiserres.dll)
+if %VER%_%PRE% == 11_ISO (del /f /q %SOURCES%\appraiserres.dll 2>nul & cd.>%SOURCES%\appraiserres.dll & call :canary)
 if %VER%_%MOD% == 11_SRV (set ARG=%OPT% %SRV% /Product Server)
 if %VER%_%MOD% == 11_CLI (set ARG=%OPT% %CLI%)
 %SOURCES%\WindowsUpdateBox.exe %ARG%
-if %errorlevel% == %restart_application% %SOURCES%\WindowsUpdateBox.exe %ARG%
+if %errorlevel% == %restart_application% (call :canary & %SOURCES%\WindowsUpdateBox.exe %ARG%)
+exit /b
+
+:canary iso skip 2nd tpm check by AveYo  
+set C=  $X='%SOURCES%\hwreqchk.dll'; $Y='SQ_TpmVersion GTE 1'; $Z='SQ_TpmVersion GTE 0'; if (test-path $X) { 
+set C=%C%  try { takeown.exe /f $X /a; icacls.exe $X /grant *S-1-5-32-544:f; attrib -R -S $X; [io.file]::OpenWrite($X).close() }
+set C=%C%  catch { return }; $R=[Text.Encoding]::UTF8.GetBytes($Z); $l=$R.Length; $i=2; $w=!1;
+set C=%C%  $B=[io.file]::ReadAllBytes($X); $H=[BitConverter]::ToString($B) -replace '-';
+set C=%C%  $S=[BitConverter]::ToString([Text.Encoding]::UTF8.GetBytes($Y)) -replace '-';
+set C=%C%  do { $i=$H.IndexOf($S, $i + 2); if ($i -gt 0) { $w=!0; for ($k=0; $k -lt $l; $k++) { $B[$k + $i / 2]=$R[$k] } } }
+set C=%C%  until ($i -lt 1); if ($w) { [io.file]::WriteAllBytes($X, $B); [GC]::Collect() } }
+if %VER%_%PRE% == 11_ISO powershell -nop -c iex($env:C) >nul 2>nul
 exit /b
 
 :setup
@@ -51,7 +62,7 @@ reg add "%IFEO%\SetupHost.exe" /f /v UseFilter /d 1 /t reg_dword >nul
 reg add "%IFEO%\SetupHost.exe\0" /f /v FilterFullPath /d "%SystemDrive%\$WINDOWS.~BT\Sources\SetupHost.exe" >nul
 reg add "%IFEO%\SetupHost.exe\0" /f /v Debugger /d "%SystemDrive%\Scripts\get11.cmd" >nul
 echo;
-%<%:f0 " Skip TPM Check on Dynamic Update V12 "%>>% & %<%:2f " INSTALLED "%>>% & %<%:f0 " run again to remove "%>%
+%<%:f0 " Skip TPM Check on Dynamic Update V13 "%>>% & %<%:2f " INSTALLED "%>>% & %<%:f0 " run again to remove "%>%
 if /i "%CLI%"=="" timeout /t 7
 exit /b
 
@@ -59,7 +70,7 @@ exit /b
 del /f /q "%SystemDrive%\Scripts\get11.cmd" "%Public%\get11.cmd" "%ProgramData%\get11.cmd" >nul 2>nul
 reg delete "%IFEO%\SetupHost.exe" /f >nul 2>nul
 echo;
-%<%:f0 " Skip TPM Check on Dynamic Update V12 "%>>% & %<%:df " REMOVED "%>>% & %<%:f0 " run again to install "%>%
+%<%:f0 " Skip TPM Check on Dynamic Update V13 "%>>% & %<%:df " REMOVED "%>>% & %<%:f0 " run again to install "%>%
 if /i "%CLI%"=="" timeout /t 7
 exit /b
 
